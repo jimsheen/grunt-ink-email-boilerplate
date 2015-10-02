615d6c0c75b6ec2439f983670997f61eac99fe85
Jim <jimsheen@hotmail.com>
2015-10-02 09:45:54 +0100

    Install and package.json changed

--- scripts/install.js
+++ scripts/install.js
@@ -6,7 +6,31 @@ bowercc_config = JSON.stringify({
     }
 });
 
-fs.writeFile(".bowerrc", bowercc_config, function(err) {
+bower_config = JSON.stringify({
+    "name": "jims-wordpress-setup",
+    "version": "1.0.0",
+    "authors": [
+        "Jim Sheen"
+    ],
+    "license": "MIT",
+    "ignore": [
+        "**/.*",
+        "node_modules",
+        "bower_components",
+        "test",
+        "tests"
+    ]
+});
+
+
+fs.outputFile("bower.json", bower_config, function(err) {
+    if (err) {
+        return console.log(err);
+    }
+})
+
+
+fs.outputFile(".bowerrc", bower_config, function(err) {
     if (err) {
         return console.log(err);
     }
